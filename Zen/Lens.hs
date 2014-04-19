e6b81290202371470bfdebb8509aa0d1658e8340
Jochen Keil <jochen.keil@gmail.com>
2014-04-19 12:14:52 +0200

    Clean up Lens module

--- Zen/Lens.hs
+++ Zen/Lens.hs
@@ -1,4 +1,4 @@
--- {-# LANGUAGE TemplateHaskell #-}
+module Lens where
 
 -- vim macro for lens function type
 -- mx{jwyw'xdf_2f "0PxbPaLens j0
@@ -7,11 +7,8 @@
 -- dt_ywPa= lens pa(\v d = d { f:C= v })0x
 
 import Prelude hiding ((.), id)
-import Data.Word
 import Control.Category
 import Control.Monad.State
--- import Data.Lens.Common
--- import Control.Lens ((-=))
 
 data Lens a b = Lens { getL :: a -> b, setL :: b -> a -> a }
 
@@ -33,7 +30,7 @@ modL :: Lens a b -> (b -> b) -> a -> a
 modL l f v = setL l (f $ getL l v) v
 
 getL' :: (MonadState a m, Monad m) => Lens a b -> m b
-getL' l = get >>= return . getL l
+getL' l = liftM (getL l) get
 
 setL' :: (MonadState a m, Monad m) => Lens a b -> b -> m ()
 setL' l v = get >>= put . setL l v
@@ -49,83 +46,5 @@ modL' l f = get >>= put . modL l f
 l %:= f = modL' l f
 infix 4 %:=
 
--- (~=) :: (MonadState a m, Monad m) => Lens a b -> m b -> m a -> m a
--- (~=) l v = get >>= return . getL l
-
-
 lens :: (a -> b) -> (b -> a -> a) -> Lens a b
 lens = Lens
-
-data Position = Position { _x :: Int, _y :: Int }
-    deriving (Eq, Read, Show)
-
-x :: Lens Position Int
-x = lens _x (\x' p -> p { _x = x' })
-
-y :: Lens Position Int
-y = lens _y (\y' p -> p { _y = y' })
-
--- makeLenses ''Position
-
-data Dimension = Dimension { _width :: Word, _height :: Word }
-    deriving (Eq, Read, Show)
-
-width :: Lens Dimension Word
-width = lens _width (\width' p -> p { _width = width' })
-
-height :: Lens Dimension Word
-height = lens _height (\height' p -> p { _height = height' })
-
--- makeLenses ''Dimension
-
-data Geometry = Geometry { _position :: Position, _dimension :: Dimension }
-    deriving (Eq, Read, Show)
-
-position :: Lens Geometry Position
-position = lens _position (\position' p -> p { _position = position' })
-
-dimension :: Lens Geometry Dimension
-dimension = lens _dimension (\dimension' p -> p { _dimension = dimension' })
-
--- makeLenses ''Geometry
-
--- position :: Functor f => (Position -> f Position) -> Geometry -> f Geometry
--- position f (Geometry position' geometry') = fmap (\position'' -> Geometry position'' geometry') (f position')
-
--- x :: Functor f => (Int -> f Int) -> Position -> f Position
--- x f (Position x' y') = fmap (\x'' -> Position x'' y') (f x')
-
--- -- quux :: Lens (Foo a) (Foo b) a b
--- quux :: Functor f => (a -> f b) -> Foo a -> f (Foo b)
--- quux f (Foo a b c) = fmap (Foo a b) (f c)
-
-
-runTestLens :: StateT Geometry IO ()
-runTestLens = do
-
-    (x . position) %:= (+1)
-    (width . dimension) %:= (+100)
-    getL' (x . position) >>= liftIO . print
-
-
-    -- position . x -= 1
-    -- position . y -= 1
-    -- dimension . width += 100
-    -- dimension . height += 100
-    get >>= liftIO . putStrLn . ("runTestLens: " ++) . show
-    -- doSomethingElse
-
--- doSomethingElse :: StateT Geometry IO ()
--- doSomethingElse = do
---     position . x ^. (+ 1) >>= liftIO . print
---     -- position ^= Position 13 42
-
-initialGeometry :: Geometry
-initialGeometry = Geometry (Position 1 2) (Dimension 100 200)
-
-test :: IO ()
-test = do
-    let xGeometry = _x . _position
-    let x' = xGeometry initialGeometry
-    print x'
-    execStateT runTestLens initialGeometry >>= print
