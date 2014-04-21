3a10171777ddff89f9c7fa2525d755aa01637824
Jochen Keil <jochen.keil@gmail.com>
2014-04-21 11:39:46 +0200

    Keysym to Keycode mapping

--- Zen/Util.hs
+++ Zen/Util.hs
@@ -2,17 +2,18 @@
 
 module Util where
 
-import Data.Maybe
-
 import qualified Data.Map as M
 import Control.Monad.Writer
 import Control.Applicative
 import Graphics.XHB
-import Graphics.X11.Types hiding (Connection)
 import Types
 
 fi :: (Integral a, Num b) => a -> b
 fi = fromIntegral
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (a:_) = Just a
 
 convertXid :: (XidLike a, XidLike b) => a -> b
 convertXid = fromXid . toXid
@@ -51,67 +52,17 @@ subLists n lst = take n lst : subLists n (drop n lst)
 -- http://cgit.freedesktop.org/~arnau/xcb-util/tree/keysyms/keysyms.c
 -- -> xcb_key_symbols_get_keysym
 
-getKeycode' :: Connection -> IO (M.Map KEYCODE [KEYSYM])
-getKeycode' c = keycodes c =<<
-    getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1)
-    where setup = connectionSetup c
-          min_keycode = min_keycode_Setup setup
-          max_keycode = max_keycode_Setup setup
-
-getKeycode :: Connection -> KEYSYM -> IO (Maybe KEYCODE)
-getKeycode c keysym = do
-    let setup = connectionSetup c
-        min_keycode = min_keycode_Setup setup
-        max_keycode = max_keycode_Setup setup
-
-    fmap (+ min_keycode) . getKeycodeFromReply <$> (getReply =<<
-        getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1))
-
-    where
-    getKeycodeFromReply :: Either SomeError GetKeyboardMappingReply -> Maybe KEYCODE
-    getKeycodeFromReply (Left _) = Nothing
-    getKeycodeFromReply (Right reply) = findKeycode (subLists (fi ks_per_kc) ks)
-        where ks = keysyms_GetKeyboardMappingReply reply
-              ks_per_kc = keysyms_per_keycode_GetKeyboardMappingReply reply
-
-    findKeycode :: [[KEYSYM]] -> Maybe KEYCODE
-    findKeycode = findKeycode' 0
-        where
-        findKeycode' n (keysyms:keysymslst)
-            | keysym `elem` keysyms = Just n
-            | otherwise             = findKeycode' (n+1) keysymslst
-        findKeycode' _ _ = Nothing
-
-
-    -- where
-    -- getKeycodeFromReply (Left _) = Nothing
-    -- getKeycodeFromReply (Right reply) = findKeycode (subLists (fi ks_per_kc) ks)
-    --     where ks = keysyms_GetKeyboardMappingReply reply
-    --           ks_per_kc = keysyms_per_keycode_GetKeyboardMappingReply reply
-
-keycodes :: Connection -> Receipt GetKeyboardMappingReply -> IO (M.Map KEYCODE [KEYSYM])
-keycodes c receipt = keycodes' <$> getReply receipt
--- keycodes c (Right reply) = M.fromList $ zip [min_keycode ..] keysyms
-    -- fmap (+ min_keycode) . getKeycodeFromReply <$> (getReply =<<
-    --     getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1))
-
+keyboardMapping :: Connection -> Receipt GetKeyboardMappingReply
+                -> IO (M.Map KEYCODE [KEYSYM])
+keyboardMapping c receipt = keycodes' <$> getReply receipt
     where
     keycodes' (Left _) = M.empty
     keycodes' (Right reply) =
         let min_keycode = min_keycode_Setup $ connectionSetup c
-            keysyms_per_keycode = fi $ keysyms_per_keycode_GetKeyboardMappingReply reply
-            keysyms = subLists keysyms_per_keycode $ keysyms_GetKeyboardMappingReply reply
+            ks_per_kc = fi $ keysyms_per_keycode_GetKeyboardMappingReply reply
+            keysyms = subLists ks_per_kc $ keysyms_GetKeyboardMappingReply reply
         in M.fromList $ zip [min_keycode ..] keysyms
 
-    -- getKeycodeFromReply :: GetKeyboardMappingReply -> Maybe KEYCODE
-    -- getKeycodeFromReply reply = findKeycode (subLists (fi ks_per_kc) ks)
-    --     where ks = keysyms_GetKeyboardMappingReply reply
-    --           ks_per_kc = keysyms_per_keycode_GetKeyboardMappingReply reply
 
-    -- findKeycode :: [[KEYSYM]] -> Maybe KEYCODE
-    -- findKeycode = findKeycode' 0
-    --     where
-    --     findKeycode' n (keysyms:keysymslst)
-    --         | keysym `elem` keysyms = Just n
-    --         | otherwise             = findKeycode' (n+1) keysymslst
-    --     findKeycode' _ _ = Nothing
+keysymToKeycode :: KEYSYM -> M.Map KEYCODE [KEYSYM] -> Maybe KEYCODE
+keysymToKeycode keysym = safeHead . M.keys . M.filter (fi keysym `elem`)
