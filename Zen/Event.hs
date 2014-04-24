1c12b1d2e9b8f395892dfd895385a33ac05696ea
Jochen Keil <jochen.keil@gmail.com>
2014-04-24 20:11:45 +0200

    Current state of Event.hs

--- Zen/Event.hs
+++ Zen/Event.hs
@@ -7,28 +7,31 @@ import qualified Data.Map as M
 import Data.Word
 import Data.Maybe
 import Control.Monad
+import Control.Monad.State
 import Control.Applicative
 import Graphics.XHB
 import Graphics.X11.Types hiding (Connection)
 
+import Log
+import Lens
 import Util
-import Types
+import Types hiding (focus)
+-- import Core
 import Client
 
-
-handler :: [EventHandler (Z Bool)]
-handler =
-    [ EventHandler handleCreateNotify
-    , EventHandler handleDestroyNotify
-    , EventHandler handleMapRequest
-    , EventHandler handleConfigureRequest
-    , EventHandler handleEnterNotify
-    , EventHandler handleLeaveNotify
-    , EventHandler handleButtonPress
-    , EventHandler handleButtonRelease
-    , EventHandler handleKeyPress
-    , EventHandler handleKeyRelease
-    ]
+-- handler :: [EventHandler (Z ())]
+-- handler =
+--     [ EventHandler handleMapRequest
+--     , EventHandler handleConfigureRequest
+--     -- , EventHandler handleCreateNotify
+--     -- , EventHandler handleDestroyNotify
+--     -- , EventHandler handleEnterNotify
+--     -- , EventHandler handleLeaveNotify
+--     -- , EventHandler handleButtonPress
+--     -- , EventHandler handleButtonRelease
+--     -- , EventHandler handleKeyPress
+--     -- , EventHandler handleKeyRelease
+--     ]
 
 
 handleError :: Maybe SomeError -> Z ()
@@ -37,43 +40,235 @@ handleError (Just se) = toLog $ "ERROR: " ++ show se
 
 
 popHandler :: Z ()
-popHandler = do
-    eventHandler <.> config %:= popOne
+popHandler = eventHandler %:= safePop
     where
-    popOne (_:es) = es
-    popOne _ = []
+    safePop (_:es) = es
+    safePop _ = []
 
 
-pushHandler :: EventHandler (Z Bool) -> Z ()
-pushHandler eh = eventHandler <.> config %:= (eh :)
+pushHandler :: EventHandler (Z ()) -> Z ()
+pushHandler eh = eventHandler %:= (eh :)
 
 
 dispatch :: SomeEvent -> Z ()
-dispatch e = getsL (eventHandler <.> config) >>= runHandler
+-- dispatch e = eventHandler $*> mapM_ (\(EventHandler h) -> run' (fromEvent e) h)
+-- dispatch e = eventHandler $*> mapM_ (\(EventHandler h) -> run' (fromEvent e) h)
+-- dispatch e = eventHandler $*> mapM_ (run' (fromEvent e))
+dispatch e = mapM_ try <*$ eventHandler
     where
-    runHandler :: [EventHandler (Z Bool)] -> Z ()
-    runHandler (EventHandler h:hs) = do
-        taken <- fromMaybe (return False) $ liftM h $ fromEvent e
-        unless taken $ runHandler hs
-    runHandler _ = return ()
+    try :: EventHandler (Z ()) -> Z ()
+    try (EventHandler handler) = void $ whenJust (fromEvent e) handler
+
+    -- try :: [EventHandler (Z ())] -> Z ()
+    -- try hs = forM_ hs $ \(EventHandler h) -> run h (fromEvent e)
+    -- try (EventHandler h:hs) = run h (fromEvent e) >> try hs
+    -- try _ = return ()
+
+    -- run :: (e -> Z ()) -> Maybe e -> Z ()
+    -- run _ Nothing = return ()
+    -- run handle (Just event) = handle event
+
+    -- foo :: Event e => e -> EventHandler (Z ()) -> Z ()
+    -- foo event (EventHandler handler) = handler event
+
+    -- run (Just event) handler = handler event
+
+-- handler :: [EventHandler (Z ())]
+-- handler =
+--     [ EventHandler handleMapRequest
+--     , EventHandler handleConfigureRequest
+--     -- , EventHandler handleCreateNotify
+--     -- , EventHandler handleDestroyNotify
+--     -- , EventHandler handleEnterNotify
+--     -- , EventHandler handleLeaveNotify
+--     -- , EventHandler handleButtonPress
+--     -- , EventHandler handleButtonRelease
+--     -- , EventHandler handleKeyPress
+--     -- , EventHandler handleKeyRelease
+--     ]
+
+    -- [ EventHandler $ \e -> do
+    --     let event_window = window_MapRequestEvent e
+    --     toLog "MapRequestEvent"
+    --     io $ print e
+    --     connection $-> io . flip mapWindow event_window
+    --     return True
+
+    -- , EventHandler $ \e -> do
+    --     let win = window_ConfigureRequestEvent e
+    --         values = toValueParam $ copyValues e (value_mask_ConfigureRequestEvent e)
+    --     toLog "ConfigureRequestEvent"
+    --     c <- asksL connection
+    --     io $ do
+    --         print e
+    --         configureWindow c win values
+    --     io (pollForError c) >>= handleError
+    --     return True
 
 
-handleCreateNotify :: CreateNotifyEvent -> Z Bool
+copyValues :: ConfigureRequestEvent -> [ConfigWindow] -> [(ConfigWindow, Word32)]
+copyValues e (ConfigWindowX           : ms) =
+    (ConfigWindowX,           fi $ x_ConfigureRequestEvent e)               : copyValues e ms
+copyValues e (ConfigWindowY           : ms) =
+    (ConfigWindowY,           fi $ y_ConfigureRequestEvent e)               : copyValues e ms
+copyValues e (ConfigWindowWidth       : ms) =
+    (ConfigWindowWidth,       fi $ width_ConfigureRequestEvent e)           : copyValues e ms
+copyValues e (ConfigWindowHeight      : ms) =
+    (ConfigWindowHeight,      fi $ height_ConfigureRequestEvent e)          : copyValues e ms
+copyValues e (ConfigWindowBorderWidth : ms) =
+    (ConfigWindowBorderWidth, fi $ border_width_ConfigureRequestEvent e)    : copyValues e ms
+copyValues e (ConfigWindowSibling     : ms) =
+    (ConfigWindowSibling,     convertXid $ sibling_ConfigureRequestEvent e) : copyValues e ms
+copyValues e (ConfigWindowStackMode   : ms) =
+    (ConfigWindowStackMode,   toValue $ stack_mode_ConfigureRequestEvent e) : copyValues e ms
+copyValues _ _ = []
+
+
+handleMapRequest :: MapRequestEvent -> Z ()
+handleMapRequest e = do
+    toLog "MapRequestEvent"
+    connection $-> io . flip mapWindow (window_MapRequestEvent e)
+
+
+handleConfigureRequest :: ConfigureRequestEvent -> Z ()
+handleConfigureRequest e = do
+    -- connection $-> \c -> io $ configureWindow c window values
+    -- configureGeometry window (Just $ Position x' y') (Just $ Dimension w' h')
+    -- toLog "ConfigureRequestEvent"
+    -- io (pollForError c) >>= handleError
+    configure window values
+    where
+    -- x'     = fi $ x_ConfigureRequestEvent e
+    -- y'     = fi $ y_ConfigureRequestEvent e
+    -- w'     = fi $ width_ConfigureRequestEvent e
+    -- h'     = fi $ height_ConfigureRequestEvent e
+    window = window_ConfigureRequestEvent e
+    values = copyValues e (value_mask_ConfigureRequestEvent e)
+    -- values = toValueParam $ copyValues e (value_mask_ConfigureRequestEvent e)
+
+
+handleCreateNotify :: CreateNotifyEvent -> Z ()
+handleCreateNotify e = manage window g
+    where window  = window_CreateNotifyEvent e
+          x'      = fi $ x_CreateNotifyEvent e
+          y'      = fi $ y_CreateNotifyEvent e
+          width'  = fi $ width_CreateNotifyEvent e
+          height' = fi $ height_CreateNotifyEvent e
+          g       = Geometry (Position x' y') (Dimension width' height')
+    -- toLog "CreateNotifyEvent"
+
+
+handleDestroyNotify :: DestroyNotifyEvent -> Z ()
+handleDestroyNotify = unmanage . window_DestroyNotifyEvent
+    -- toLog "DestroyNotifyEvent"
+    -- removeWindow (window_DestroyNotifyEvent e) >> return True
+
+
+handleEnterNotify :: EnterNotifyEvent -> Z ()
+handleEnterNotify e = when (not isInferior) $ focus (event_EnterNotifyEvent e)
+    -- toLog "EnterNotifyEvent"
+    where isInferior = NotifyDetailInferior == detail_EnterNotifyEvent e
+
+
+handleLeaveNotify :: LeaveNotifyEvent -> Z ()
+handleLeaveNotify e = when (not isInferior) $ focus (event_LeaveNotifyEvent e)
+    -- toLog "LeaveNotifyEvent"
+    where isInferior = NotifyDetailInferior == detail_LeaveNotifyEvent e
+    -- case detail_LeaveNotifyEvent e of
+    --     NotifyDetailInferior -> return False
+    --     _ -> withClient (event_LeaveNotifyEvent e) unfocus >> return True
+
+handleButtonPress :: ButtonPressEvent -> Z ()
+handleButtonPress e = do
+    toLog "ButtonPressEvent"
+    mask <- asksL (modMask <.> config)
+    when (toBit mask `notElem` map toBit (state_ButtonPressEvent e)) $ do
+        fs <- asksL (buttonPressHandler <.> config)
+        void $ whenJust (M.lookup (fromValue $ detail_ButtonPressEvent e) fs) $ \f -> f e
+
+
+handleButtonRelease :: ButtonReleaseEvent -> Z ()
+handleButtonRelease e = do
+    toLog "ButtonReleaseEvent"
+    mask <- asksL (modMask <.> config)
+    when (toBit mask `notElem` map toBit (state_ButtonReleaseEvent e)) $ do
+        fs <- asksL (buttonReleaseHandler <.> config)
+        void $ whenJust (M.lookup (fromValue $ detail_ButtonReleaseEvent e) fs) $ \f -> f e
+
+
+handleKeyPress :: KeyPressEvent -> Z ()
+handleKeyPress e = do
+    toLog "KeyPressEvent"
+--     toLog $ show e
+--
+--     c <- asksL connection
+--
+--     let setup = connectionSetup c
+--         min_keycode = min_keycode_Setup setup
+--         max_keycode = max_keycode_Setup setup
+--
+--     kbdmap <- io (keyboardMapping c =<<
+--         getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1))
+--
+--     -- modmap <- getModmap <$> io (getModifierMapping c >>= getReply)
+--
+--     -- let numlock = flip L.elemIndex modmap
+--     -- <$> (keysymToKeycode (fi xK_Num_Lock) kbdmap >>= \kc -> L.find (kc `elem`) modmap)
+--     --     capslock = flip L.elemIndex modmap
+-- --     <$> (keysymToKeycode (fi xK_Caps_Lock) kbdmap >>= \kc -> L.find (kc `elem`) modmap)
+--     --     state = state_KeyPressEvent e
+--
+--
+--     when (keysymToKeycode (fi xK_Alt_L) kbdmap == Just (detail_KeyPressEvent e)) $ do
+--         forM_ (map fi [xK_Tab]) $ \keysym -> do
+--             whenJust (keysymToKeycode keysym kbdmap) $ \keycode ->
+--                 io $ grabKey c $ MkGrabKey True (getRoot c) [ModMask1] keycode
+--                                                GrabModeAsync GrabModeAsync
+--
+--     when (keysymToKeycode (fi xK_Tab) kbdmap == Just (detail_KeyPressEvent e)) $ do
+--         if KeyButMaskShift `elem` state_KeyPressEvent e
+--             then toLog "Focus prev!"
+--             else toLog "Focus next!"
+
+
+
+handleKeyRelease :: KeyReleaseEvent -> Z ()
+handleKeyRelease e = do
+    toLog "KeyReleaseEvent"
+    toLog $ show e
+--
+--     c <- asksL connection
+--
+--     let setup = connectionSetup c
+--         min_keycode = min_keycode_Setup setup
+--         max_keycode = max_keycode_Setup setup
+--
+--     kbdmap <- io (keyboardMapping c =<<
+--         getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1))
+--
+--     when (keysymToKeycode (fi xK_Alt_L) kbdmap == Just (detail_KeyReleaseEvent e)) $ do
+--         forM_ (map fi [xK_Tab]) $ \keysym -> do
+--             whenJust (keysymToKeycode keysym kbdmap) $ \keycode ->
+--                 io $ ungrabKey c $ MkUngrabKey keycode (getRoot c) [ModMask1]
+
+{-
+
+
+handleCreateNotify :: CreateNotifyEvent -> Z ()
 handleCreateNotify e = do
     toLog "CreateNotifyEvent"
     insertWindow window
     grabButtons window
-    return True
     where window = window_CreateNotifyEvent e
 
 
-handleDestroyNotify :: DestroyNotifyEvent -> Z Bool
+handleDestroyNotify :: DestroyNotifyEvent -> Z ()
 handleDestroyNotify e = do
     toLog "DestroyNotifyEvent"
     removeWindow (window_DestroyNotifyEvent e) >> return True
 
 
-handleEnterNotify :: EnterNotifyEvent -> Z Bool
+handleEnterNotify :: EnterNotifyEvent -> Z ()
 handleEnterNotify e = do
     toLog "EnterNotifyEvent"
     case detail_EnterNotifyEvent e of
@@ -81,7 +276,7 @@ handleEnterNotify e = do
         _ -> withClient (event_EnterNotifyEvent e) focus >> return True
 
 
-handleLeaveNotify :: LeaveNotifyEvent -> Z Bool
+handleLeaveNotify :: LeaveNotifyEvent -> Z ()
 handleLeaveNotify e = do
     toLog "LeaveNotifyEvent"
     case detail_LeaveNotifyEvent e of
@@ -89,17 +284,17 @@ handleLeaveNotify e = do
         _ -> withClient (event_LeaveNotifyEvent e) unfocus >> return True
 
 
-handleMapRequest :: MapRequestEvent -> Z Bool
+
+handleMapRequest :: MapRequestEvent -> Z ()
 handleMapRequest e = do
     toLog "MapRequestEvent"
     io $ print e
-    withConnection $ io . flip mapWindow event_window
-    return True
+    connection $-> io . flip mapWindow event_window
     where
     event_window = window_MapRequestEvent e
 
 
-handleConfigureRequest :: ConfigureRequestEvent -> Z Bool
+handleConfigureRequest :: ConfigureRequestEvent -> Z ()
 handleConfigureRequest e = do
     toLog "ConfigureRequestEvent"
     c <- asksL connection
@@ -107,106 +302,37 @@ handleConfigureRequest e = do
         print e
         configureWindow c win values
     io (pollForError c) >>= handleError
-    return True
     where
     win = window_ConfigureRequestEvent e
     values = toValueParam $ copyValues (value_mask_ConfigureRequestEvent e)
 
     copyValues :: [ConfigWindow] -> [(ConfigWindow, Word32)]
-    copyValues (ConfigWindowX           : ms) = (ConfigWindowX,           fi $ x_ConfigureRequestEvent e)                    : copyValues ms
-    copyValues (ConfigWindowY           : ms) = (ConfigWindowY,           fi $ y_ConfigureRequestEvent e)                    : copyValues ms
-    copyValues (ConfigWindowWidth       : ms) = (ConfigWindowWidth,       fi $ width_ConfigureRequestEvent e)                : copyValues ms
-    copyValues (ConfigWindowHeight      : ms) = (ConfigWindowHeight,      fi $ height_ConfigureRequestEvent e)               : copyValues ms
-    copyValues (ConfigWindowBorderWidth : ms) = (ConfigWindowBorderWidth, fi $ border_width_ConfigureRequestEvent e)         : copyValues ms
-    copyValues (ConfigWindowSibling     : ms) = (ConfigWindowSibling,     convertXid $ sibling_ConfigureRequestEvent e) : copyValues ms
-    copyValues (ConfigWindowStackMode   : ms) = (ConfigWindowStackMode,   toValue $ stack_mode_ConfigureRequestEvent e)      : copyValues ms
+    copyValues (ConfigWindowX           : ms) =
+        (ConfigWindowX,           fi $ x_ConfigureRequestEvent e)               : copyValues ms
+    copyValues (ConfigWindowY           : ms) =
+        (ConfigWindowY,           fi $ y_ConfigureRequestEvent e)               : copyValues ms
+    copyValues (ConfigWindowWidth       : ms) =
+        (ConfigWindowWidth,       fi $ width_ConfigureRequestEvent e)           : copyValues ms
+    copyValues (ConfigWindowHeight      : ms) =
+        (ConfigWindowHeight,      fi $ height_ConfigureRequestEvent e)          : copyValues ms
+    copyValues (ConfigWindowBorderWidth : ms) =
+        (ConfigWindowBorderWidth, fi $ border_width_ConfigureRequestEvent e)    : copyValues ms
+    copyValues (ConfigWindowSibling     : ms) =
+        (ConfigWindowSibling,     convertXid $ sibling_ConfigureRequestEvent e) : copyValues ms
+    copyValues (ConfigWindowStackMode   : ms) =
+        (ConfigWindowStackMode,   toValue $ stack_mode_ConfigureRequestEvent e) : copyValues ms
     copyValues _ = []
+-
+-}
 
 
-handleButtonPress :: ButtonPressEvent -> Z Bool
-handleButtonPress e = do
-    toLog "ButtonPressEvent"
-    mask <- getsL (modMask <.> config)
-    if toBit mask `notElem` map toBit (state_ButtonPressEvent e)
-        then return False
-        else do
-            fs <- getsL (buttonPressHandler <.> config)
-            void $ whenJust (M.lookup (fromValue $ detail_ButtonPressEvent e) fs) $ \f -> f e
-            return True
+{-
 
 
-handleButtonRelease :: ButtonReleaseEvent -> Z Bool
-handleButtonRelease e = do
-    toLog "ButtonReleaseEvent"
-    mask <- getsL (modMask <.> config)
-    if toBit mask `notElem` map toBit (state_ButtonReleaseEvent e)
-        then return False
-        else do
-            fs <- getsL (buttonReleaseHandler <.> config)
-            void $ whenJust (M.lookup (fromValue $ detail_ButtonReleaseEvent e) fs) $ \f -> f e
-            return True
 
-
-handleKeyPress :: KeyPressEvent -> Z Bool
-handleKeyPress e = do
-    toLog "KeyPressEvent"
-    toLog $ show e
-
-    c <- asksL connection
-
-    let setup = connectionSetup c
-        min_keycode = min_keycode_Setup setup
-        max_keycode = max_keycode_Setup setup
-
-    kbdmap <- io (keyboardMapping c =<<
-        getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1))
-
-    -- modmap <- getModmap <$> io (getModifierMapping c >>= getReply)
-
-    -- let numlock = flip L.elemIndex modmap <$> (keysymToKeycode (fi xK_Num_Lock) kbdmap >>= \kc -> L.find (kc `elem`) modmap)
-    --     capslock = flip L.elemIndex modmap <$> (keysymToKeycode (fi xK_Caps_Lock) kbdmap >>= \kc -> L.find (kc `elem`) modmap)
-    --     state = state_KeyPressEvent e
-
-
-    when (keysymToKeycode (fi xK_Alt_L) kbdmap == Just (detail_KeyPressEvent e)) $ do
-        forM_ (map fi [xK_Tab]) $ \keysym -> do
-            whenJust (keysymToKeycode keysym kbdmap) $ \keycode ->
-                io $ grabKey c $ MkGrabKey True (getRoot c) [ModMask1] keycode
-                                               GrabModeAsync GrabModeAsync
-
-    when (keysymToKeycode (fi xK_Tab) kbdmap == Just (detail_KeyPressEvent e)) $ do
-        if KeyButMaskShift `elem` state_KeyPressEvent e
-            then toLog "Focus prev!"
-            else toLog "Focus next!"
-
-    return True
-
-
-handleKeyRelease :: KeyReleaseEvent -> Z Bool
-handleKeyRelease e = do
-    toLog "KeyReleaseEvent"
-    toLog $ show e
-
-    c <- asksL connection
-
-    let setup = connectionSetup c
-        min_keycode = min_keycode_Setup setup
-        max_keycode = max_keycode_Setup setup
-
-    kbdmap <- io (keyboardMapping c =<<
-        getKeyboardMapping c min_keycode (max_keycode - min_keycode + 1))
-
-    when (keysymToKeycode (fi xK_Alt_L) kbdmap == Just (detail_KeyReleaseEvent e)) $ do
-        forM_ (map fi [xK_Tab]) $ \keysym -> do
-            whenJust (keysymToKeycode keysym kbdmap) $ \keycode ->
-                io $ ungrabKey c $ MkUngrabKey keycode (getRoot c) [ModMask1]
-    return True
-
-
-moveWindow :: MotionNotifyEvent -> Z Bool
+moveWindow :: MotionNotifyEvent -> Z ()
 moveWindow e = do
     withConnection $ \c -> getsL pointer >>= configure c window
-    return True
     where
     root_x = fi $ root_x_MotionNotifyEvent e
     root_y = fi $ root_y_MotionNotifyEvent e
@@ -216,13 +342,12 @@ moveWindow e = do
                                                (ConfigWindowY, root_y - fi y')]
 
 
-resizeWindow :: MotionNotifyEvent -> Z Bool
+resizeWindow :: MotionNotifyEvent -> Z ()
 resizeWindow e = do
     toLog "resizeWindow"
     -- TODO: do this with focused client
     void $ withConnection $ withClient window . configure
 
-    return True
 
     where
     window = event_MotionNotifyEvent e
@@ -307,3 +432,5 @@ defaultButtonReleaseHandler = M.fromList
         popHandler
       )
     ]
+
+    -}
