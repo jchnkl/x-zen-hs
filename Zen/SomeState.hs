d32b7ffb8b62cd102d01251e054ddf3d18848e3c
Jochen Keil <jochen.keil@gmail.com>
2014-05-04 21:32:47 +0200

    Implement unified configuration for components

--- Zen/SomeState.hs
+++ Zen/SomeState.hs
@@ -1,7 +1,9 @@
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
+{-# LANGUAGE RankNTypes #-}
 
 module SomeState where
 
+import Data.Typeable
 import Control.Monad.State
 import Control.Monad.Reader
 import Control.Monad.Writer
@@ -10,6 +12,13 @@ import Graphics.XHB (SomeEvent, fromEvent)
 import Log
 import Util
 import Types
+
+
+getConfig :: Typeable a => [ComponentConfig] -> Maybe a
+getConfig (ComponentConfig c:cs) = case cast c of
+    Just c' -> Just c'
+    Nothing -> getConfig cs
+getConfig _ = Nothing
 
 
 eventDispatcher :: (Functor m, Monad m) => [EventHandler (m ())] -> SomeEvent -> m ()
