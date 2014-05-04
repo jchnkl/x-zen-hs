fe660699f3e5d876ee6c454be0b977dcbcdc11e8
Jochen Keil <jochen.keil@gmail.com>
2014-05-04 21:45:56 +0200

    Clean up Window module (grabButtons is in Button module)

--- Zen/Window.hs
+++ Zen/Window.hs
@@ -3,19 +3,13 @@
 module Window where
 
 import Data.Word
-import Data.Maybe (catMaybes)
-import qualified Data.Map as M
-import qualified Data.List as L
 import Control.Monad
 import Control.Monad.IO.Class
 import Graphics.XHB
-import Graphics.X11.Types (xK_Num_Lock, xK_Caps_Lock)
 
 import Lens
 import Util
 import Types
-import Keyboard
-
 
 
 setBorderColor :: MonadIO m => WindowId -> Word -> Z m ()
@@ -52,28 +46,6 @@ raise = flip configure [(ConfigWindowStackMode, toValue StackModeAbove)]
 lower :: MonadIO m => WindowId -> Z m ()
 lower = flip configure [(ConfigWindowStackMode, toValue StackModeBelow)]
 
-
-grabButtons :: MonadIO m => WindowId -> Z m ()
-grabButtons window = connection $-> \c -> do
-    modmask <- askL (config . modMask)
-    kbdmap <- askL keyboardMap
-    modmap <- askL modifierMap
-    eventmask <- askL buttonMask
-    let nl = catMaybes [fmap (fromBit . toValue) $ keysymToModifier kbdmap modmap (fi xK_Num_Lock)]
-        cl = catMaybes [fmap (fromBit . toValue) $ keysymToModifier kbdmap modmap (fi xK_Caps_Lock)]
-        -- TODO: separate function
-        combos m b = L.nub $ zip (m : map (m ++) [nl, cl, nl ++ cl]) [b, b ..]
-
-    buttons <- asksL (config . buttonHandler) (M.keys)
-
-    forM_ buttons $ \(m, b) -> mapM_ (grab c eventmask) $ combos (modmask ++ m) b
-
-    where
-    grab c eventmask (mask, button) = do
-        io $ grabButton c $ MkGrabButton True window eventmask
-                            GrabModeAsync GrabModeAsync
-                            (convertXid xidNone) (convertXid xidNone)
-                            button mask
 
 configure :: MonadIO m => WindowId -> [(ConfigWindow, Word32)] -> Z m ()
 configure w vs = connection $-> \c -> io $ configureWindow c w $ toValueParam vs
