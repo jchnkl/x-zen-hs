e8027d39c07e6ea188f4e1cc4d919c974c6d80e0
Jochen Keil <jochen.keil@gmail.com>
2014-05-04 11:18:24 +0200

    stash

--- Zen/Button.hs
+++ Zen/Button.hs
@@ -26,6 +26,7 @@ type PointerState = LogWT (SetupRT (StateT (Maybe PointerMotion) IO))
 pointerState :: SomeState
 pointerState = Stateful Nothing
                       $ eventDispatcher [ EventHandler handleButtonPress
+                                        -- , EventHandler handleButtonRelease
                                         , EventHandler handleMotionNotify
                                         ]
 
@@ -54,6 +55,14 @@ handleButtonPress e = do
         win_pos g = Position (fi $ x_GetGeometryReply g) (fi $ y_GetGeometryReply g)
         win_dim g = Dimension (fi $ width_GetGeometryReply g) (fi $ height_GetGeometryReply g)
         edges = getEdges . Geometry (Position event_x event_y) . win_dim
+
+-- handleButtonRelease :: ButtonReleaseEvent -> PointerState ()
+-- handleButtonRelease e = do
+--     toLog "ButtonReleaseEvent"
+--     -- let state = state_ButtonReleaseEvent e
+--     --     button = fromValue $ detail_ButtonReleaseEvent e
+--     -- mask <- (\\) <$> (getCleanMask state) <*> askL (config . modMask)
+--     -- flip handleRelease e . M.lookup (mask, button) <-$ config . buttonHandler
 
 
 handleMotionNotify :: MotionNotifyEvent -> PointerState ()
@@ -86,3 +95,41 @@ handleMotionNotify e = get >>= handle
         Just West  -> [(ConfigWindowX, win_x g + delta_x p),
                        (ConfigWindowWidth, win_w g - delta_x p)]
         _          -> []
+
+
+
+
+{-
+-- | Rather convoluted function to resize a window from every corner/direction
+-- Uses closure for temporary state
+resizeWindow :: Maybe ((Edge, Edge), Position, Geometry)
+    -- ^ (({North, South or None},
+    --     {East, West or None}),
+    -- {Position of root window pointer event},
+    -- {Geometry of the window to be resized})
+             -> MotionNotifyEvent
+             -> Z ()
+resizeWindow Nothing _ = return ()
+resizeWindow (Just (edges, Position o_root_x o_root_y, geom)) e =
+    undefined -- configure window $ values (fst edges) ++ values (snd edges)
+    where
+    gx = fi $ geom ^. position . x
+    gy = fi $ geom ^. position . y
+    gw = fi $ geom ^. dimension . width
+    gh = fi $ geom ^. dimension . height
+
+    n_root_x = root_x_MotionNotifyEvent e
+    n_root_y = root_y_MotionNotifyEvent e
+
+    window = event_MotionNotifyEvent e
+
+    delta_x = fi n_root_x - fi o_root_x
+    delta_y = fi n_root_y - fi o_root_y
+
+    values = \case
+        North -> [(ConfigWindowY, gy + delta_y), (ConfigWindowHeight, gh - delta_y)]
+        South -> [(ConfigWindowHeight, gh + delta_y)]
+        East  -> [(ConfigWindowWidth, gw + delta_x)]
+        West  -> [(ConfigWindowX, gx + delta_x), (ConfigWindowWidth, gw - delta_x)]
+        None  -> []
+-}
