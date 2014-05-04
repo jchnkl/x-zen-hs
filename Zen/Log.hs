4195a91797d1249b9afe2074af97b118066f45dd
Jochen Keil <jochen.keil@gmail.com>
2014-05-04 11:21:24 +0200

    Generalize toLog & provide simple log pretty printer

--- Zen/Log.hs
+++ Zen/Log.hs
@@ -2,9 +2,16 @@
 
 module Log where
 
+import Data.Time (getZonedTime)
 import Control.Monad.Writer
 import Types
 
 
-toLog :: String -> Z ()
+toLog :: Monad m => String -> LogWT (SetupRT (m )) ()
 toLog s = tell [s]
+
+
+printLog :: [String] -> IO ()
+printLog ls = do
+    time <- getZonedTime
+    putStrLn . (show time ++) . ("\n" ++) . unlines . map ("\t" ++) $ ls
