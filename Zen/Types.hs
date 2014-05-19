46ec20f9a406023c48cb93fcd5aa5f4580c8c312
Jochen Keil <jochen.keil@gmail.com>
2014-05-19 11:50:14 +0200

    Cleanup

--- Zen/Types.hs
+++ Zen/Types.hs
@@ -51,128 +51,15 @@ import Lens
 import Lens.Family.Stock
 import Message -- hiding (dispatch)
 
--- class Typeable a => EventWrapper a where
---     wrapEvent :: a -> SomeEventWrapper
---     wrapEvent = SomeEventWrapper
---     unwrapEvent :: SomeEventWrapper -> Maybe a
---     unwrapEvent (SomeEventWrapper e) = cast e
-
--- data SomeEventWrapper = forall a. (EventWrapper a) => SomeEventWrapper a
--- data SomeHandler b = forall a. (Message a) => SomeHandler (a -> b)
-
--- deriving instance Typeable (Z m ())
--- deriving instance Typeable Z
-
--- class (Typeable a) => Handler a where
---     toHandler :: forall b. Typeable b => (a -> b) -> SomeHandler
---     toHandler = SomeHandler
---     fromHandler :: forall b. Typeable b => SomeHandler -> Maybe (a -> b)
---     fromHandler (SomeHandler f) = cast f
-
--- data HandlerId = HandlerId { getHandlerId :: ThreadId }
---     deriving (Show, Typeable)
-
--- data SomeChannel = forall a. Channel a => SomeChannel (TChan a)
-
--- class Typeable a => Channel a where
---     toChannel :: TChan a -> SomeChannel
---     toChannel = SomeChannel
---     fromChannel :: SomeChannel -> Maybe (TChan a)
---     fromChannel (SomeChannel c) = cast c
-
-
--- data EventSource = forall e. Channel e => EventSource (IO e)
-
-
--- runSources :: [EventSource] -> IO [(ThreadId, SomeChannel)]
--- runSources = mapM run
---     where
---     run :: EventSource -> IO (ThreadId, SomeChannel)
---     run (EventSource f) = do
---         chan <- newBroadcastTChanIO
---         tid <- forkIO $ forever $ f >>= atomically . writeTChan chan
---         return (tid, toChannel chan)
-
-
--- whenJustM :: Monad m => Maybe a -> (a -> m b) -> m (Maybe b)
--- whenJustM Nothing _ = return Nothing
--- whenJustM (Just v) f = liftM Just (f v)
-
-
--- attach :: (Monad m, Functor m) => Handler -> Z m (Maybe HandlerId)
--- attach :: Handler -> Z IO (Maybe HandlerId)
--- attach (Handler h) = do
---     channels <- asks _eventSources
-
-    -- let channel' = fromChannel $ head channels
-    -- ask >>= flip try channel'
-
-    -- return Nothing
-
-    -- where
-    -- try setup = flip whenJustM (liftIO . forkIO . run setup)
-
-    -- run setup channel = atomically (dupTChan channel)
-    --     >>= forever . (runStack setup . h =<<) . atomically . readTChan
-
-    -- runStack :: ComponentClass d m => Setup -> (Z m a) -> IO ()
-    -- runStack = undefined
-    -- dc = DummyComponent 1
-
-
--- detach :: MonadIO m => HandlerId -> Z m ()
--- detach = liftIO . killThread . getHandlerId
-
-
--- runComponents :: Setup -> IO ()
--- runComponents setup = do
---
---     -- let f = undefined :: Z m ()
---     mapM_ run cs
---
---     where
---         cs = setup ^. config . components
---
---         run (Component cdata runc s c f hs) = do
---             ((_, logs), cdata') <- runc (runStack setup f) cdata
---             return (Component cdata' runc s c f hs)
-
-
-
-
--- class EventHandler_ e d where
---     handle :: d -> e -> IO () -- Z m ()
---     -- runComponent_ :: SomeMessage -> d -> IO ()
 
 data ComponentConfig = forall a. (Show a, Typeable a) => ComponentConfig a
     deriving Typeable
 
 
--- type EventHandlerRegistry =
---     -- forall a m. (Typeable a, Monad m) => Map ThreadId (a -> Z m ())
---     forall a m.  Map ThreadId (a -> Z m ())
+data GenericHandler b
+    = forall a. Event a => EventHandler (a -> b)
+    | forall a. Message a => MessageHandler (a -> b)
 
-
--- data ComponentStore = ComponentStore
---    { eventHandler :: EventHandlerRegistry
---    , component :: Component
---    }
-
--- type ComponentStoreST = StateT ComponentStore
-
--- type Y = SetupRT (ComponentStoreST IO)
-
--- foo :: Y ()
--- foo = do
---     h <- gets (M.lookup (undefined :: ThreadId) . eventHandler)
---     liftIO $ print "foo"
-
-xwhenJustM :: Monad m => Maybe a -> (a -> m b) -> m (Maybe b)
-xwhenJustM Nothing _ = return Nothing
-xwhenJustM (Just v) f = liftM Just (f v)
-
-xwhenJustM_ :: (Functor m, Monad m) => Maybe a -> (a -> m b) -> m ()
-xwhenJustM_ v = void . xwhenJustM v
 
 
 data Component = forall m d. (Monad m, Functor m, Typeable d) => Component
@@ -184,163 +71,19 @@ data Component = forall m d. (Monad m, Functor m, Typeable d) => Component
     , onStartup :: d -> Z IO d
     -- | Function to run on shutdown
     , onShutdown :: d -> Z IO ()
-    -- -- | Update Component after every dispatched event
-    -- , postHandler :: Maybe ([r] -> Component -> Z IO Component)
-    -- | Event handler
-    -- , eventHandler :: d -> [SomeHandler (Z m ())]
-    , someHandler :: [SomeHandler_ (Z m ())]
+    -- | Generic event handler
+    , genericHandler :: [GenericHandler (Z m ())]
     }
 
-
-data ComponentEventHandler = ComponentEventHandler
-    { eventHandler :: forall m. MonadIO m => EventHandler (Z m ())
-    }
-
-data ComponentMessageHandler = ComponentMessageHandler
-    { messageHandler :: forall m. MonadIO m => [MessageHandler (Z m ())]
-    }
-
-data Component_ = Component_
-    Component
-    ComponentEventHandler
-    ComponentMessageHandler
-
-class ComponentEventHandler_ a where
-    getEventHandler :: forall m. MonadIO m => a -> EventHandler (Z m ())
-
-instance ComponentEventHandler_ ComponentEventHandler where
-    getEventHandler (ComponentEventHandler f) = f
-
-
--- class Typeable e => SomeHandlerClass e where
---     fromHandler :: SomeHandler_ -> Maybe (e -> m ())
---     -- fromHandler (SomeHandler_ f) = cast f
---     -- eventHandler_ :: forall m. MonadIO m => Maybe (a -> EventHandler (Z m ()))
---     -- messageHandler_ :: forall m. MonadIO m => Maybe (a -> MessageHandler (Z m ()))
-
-data SomeHandler_ b
-    = forall a. Event a => EventHandler_ (a -> b)
-    | forall a. Message a => MessageHandler_ (a -> b)
-
-
--- foo :: Setup
---     -> SomeEvent
---     -> Component
---     -- -> ContT () (WriterT [String] (ReaderT Setup m)) SomeEvent
---     -> ContT () IO SomeEvent
--- foo setup event (Component cdata runc u d) = ContT $ \k -> do
---     runc (runStack setup $ k event) cdata
---     return ()
---     -- k event
-
--- bar :: Connection -> IO () -- Either SomeError (IO ())
--- bar c = do
---     _ <- fmap (baz =<<) . getReply
---         =<< internAtom c (MkInternAtom False atom_name_len atom_name)
---     return ()
---     where
---     atom_name = stringToCList "foo"
---     atom_name_len = fromIntegral $ length atom_name
---     baz :: ATOM -> (Either SomeError String)
---     baz = undefined
-
--- data SomeComponentHandler
---     = forall h. EventHandler h => ComponentEventHandler h
---     | forall h. MessageHandler h => ComponentMessageHandler h
 
 class Dispatcher a where
-    dispatch :: forall m. (Monad m, Functor m) => a -> SomeHandler_ (m ()) -> m ()
-
-instance Dispatcher SomeEvent where
-    dispatch e (EventHandler_ f) = xwhenJustM_ (fromEvent e) f
-    dispatch e _                 = return ()
-
-foo :: ButtonPressEvent -> Z IO ()
-foo = undefined
-
-bar :: ButtonReleaseEvent -> Z IO ()
-bar = undefined
-
-data Dummy = Dummy deriving Typeable
-instance Message Dummy
-
-baz :: Dummy -> Z IO ()
-baz = undefined
-
-foobarbaz = [EventHandler_ foo, MessageHandler_ baz, EventHandler_ bar]
-
-myComponent :: Component
-myComponent = Component
-    { componentData = (1 :: Int)
-    , runComponent = \f i -> (,i) <$> f
-    , onStartup = const $ return 1
-    , onShutdown = const $ return ()
-    , someHandler = foobarbaz
-    }
-
-yoyoyo :: Setup -> SomeEvent -> IO Component
-yoyoyo setup event = dispatchEvent setup event myComponent
-
-dispatchEvent :: Dispatcher a => Setup -> a -> Component -> IO Component
-dispatchEvent setup event (Component cdata runc u d hs) = do
-    ((_, logs), cdata') <- runc (runStack setup $ mapM_ (dispatch event) hs) cdata
-    -- printLog logs
-    return $ Component cdata' runc u d hs
+    dispatch :: forall m. (Monad m, Functor m) => a -> GenericHandler (m ()) -> m ()
 
 
-runStack :: Setup -> WriterT w (ReaderT Setup m) a -> m (a, w)
-runStack setup f = runReaderT (runWriterT f) setup
+-- instance Dispatcher SomeEvent where
+--     dispatch e (EventHandler f) = xwhenJustM_ (fromEvent e) f
+--     dispatch e _                = return ()
 
--- myEventHandler :: Component -> [SomeHandler (Z m r)]
--- myEventHandler (Component cdata r u d h) = []
-
-
--- execComponent :: Setup -> SomeData -> Component -> IO Component
--- execComponent setup somed component@(Component cdata runc s c ehandler) = do
---     -- exec cdata [] (ehandler component) >>= update . second catMaybes
---     fmap (\(d, _) -> Component d runc s c ehandler) $ exec cdata [] (ehandler component)
---     --printLog logs
-
-    -- where
-
-    -- update (d, rs) = do
-    --     (component', logs) <- runStack setup
-    --                         $ updatec rs (Component d runc s c ehandler)
-    --     -- printLog logs
-    --     return component'
-
-    -- exec d rs []     = return (d, rs)
-    -- exec d rs (h:hs) = do
-    --     ((r, logs), d') <- runc (runStack setup $ try h) d
-    --     -- printLog logs
-    --     exec d (r:rs) hs
-
-    -- try (SomeHandler conv f) = xwhenJustM (fromData somed >>= conv) f
-
-
--- class (Typeable e) => HandlerClass e where
---     execHandler :: Handler (Z m ()) -> e -> Z m ()
-
--- deriving instance Typeable SomeEvent
--- instance HandlerClass SomeEvent where
---     execHandler (Handler h) e = h $ fromJust (fromEvent e)
-
--- attachHandler :: (HandlerClass e, Monad m) => Handler -> Z m ()
--- attachHandler :: (Monad m) => Handler (Z m ()) -> Z m ()
--- attachHandler handler@(Handler h) = do
---     setup <- ask
---     cs <- asks (_components . _config)
---     return ()
---
---     where
---     run setup comp@(Component cdata runc s c f hs) = do
---         let event = undefined :: SomeEvent
---         ((_, logs), cdata') <- runc (runStack setup (execHandler handler event)) cdata
---         return (Component cdata' runc s c f hs)
-
-
-data EventHandler b = forall a. (Event a) => EventHandler (a -> b)
-data MessageHandler b = forall a. (Message a) => MessageHandler (a -> b)
 
 data Config = Config
     { _modMask :: [ModMask]
