86bd6fe0175869f4abbb3e87cce2980f622eda63
Jochen Keil <jochen.keil@gmail.com>
2014-06-10 17:18:48 +0200

    Utility functions for window operations

--- Zen/Window.hs
+++ Zen/Window.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Window where
 
@@ -9,7 +10,15 @@ import Graphics.XHB
 
 import Lens
 import Util
-import Types
+import Types hiding (geometry)
+
+
+instance TypeConversion GetGeometryReply Geometry where
+    convert r = Geometry (Position x' y') (Dimension w' h')
+        where x' = fi $ x_GetGeometryReply r
+              y' = fi $ y_GetGeometryReply r
+              w' = fi $ width_GetGeometryReply r
+              h' = fi $ height_GetGeometryReply r
 
 
 setBorderColor :: MonadIO m => WindowId -> Word -> Z m ()
@@ -22,6 +31,11 @@ setBorderWidth :: MonadIO m => WindowId -> Word -> Z m ()
 setBorderWidth window bw = do
     let vp = toValueParam [(ConfigWindowBorderWidth, fi bw)]
     connection $-> \c -> io $ configureWindow c window vp
+
+
+changeAttributes :: (MonadIO m, BitEnum e) => WindowId -> [(e, Word32)] -> Z m ()
+changeAttributes window valueparams = connection $-> \c ->
+    io $ changeWindowAttributes c window $ toValueParam valueparams
 
 
 focus :: MonadIO m => WindowId -> Z m ()
@@ -49,3 +63,11 @@ lower = flip configure [(ConfigWindowStackMode, toValue StackModeBelow)]
 
 configure :: MonadIO m => WindowId -> [(ConfigWindow, Word32)] -> Z m ()
 configure w vs = connection $-> \c -> io $ configureWindow c w $ toValueParam vs
+
+
+attributes :: (MonadIO m, Functor m) => WindowId -> Z m (Receipt GetWindowAttributesReply)
+attributes window = connection $-> io . flip getWindowAttributes window
+
+
+geometry :: (MonadIO m, Functor m) => WindowId -> Z m (Receipt GetGeometryReply)
+geometry window = connection $-> io . flip getGeometry (convertXid window)
