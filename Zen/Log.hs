c8afd7fafbafd554bafb6bb013d8f997df7ea68d
Jochen Keil <jochen.keil@gmail.com>
2014-06-17 11:59:46 +0200

    Add typedef for log strings

--- Zen/Log.hs
+++ Zen/Log.hs
@@ -7,11 +7,14 @@ import Control.Monad.Writer
 import Types
 
 
+type Log = [String]
+
+
 toLog :: Monad m => String -> Z m ()
 toLog s = tell [s]
 
 
-printLog :: [String] -> IO ()
+printLog :: Log -> IO ()
 printLog ls = when (not $ null ls) $ do
     time <- getZonedTime
     putStrLn . (show time ++) . ("\n" ++) . unlines . map ("\t" ++) $ ls
