d15f31963b8f4921dbcb6b43f1f67f7aeca5dc55
Jochen Keil <jochen.keil@gmail.com>
2014-06-26 10:27:12 +0200

    Rewrite Controller module to run in SetupRT/ModelST monad

--- Zen/Controller.hs
+++ Zen/Controller.hs
@@ -5,7 +5,6 @@
 
 module Controller where
 
-import Control.Applicative
 import Control.Monad.State
 import Control.Monad.Reader
 import Control.Concurrent
@@ -19,54 +18,40 @@ import Types
 import Component
 
 
-xEventSource :: Setup -> IO AnyEvent
-xEventSource setup = AnyEvent <$> waitForEvent (setup ^. connection)
+xEventSource :: SetupRT IO AnyEvent
+xEventSource = askL connection >>= fmap AnyEvent . io . waitForEvent
 
 
-execAnyEvent :: Setup -> Model -> AnyEvent -> Component -> IO ((Log, Model), Component)
-execAnyEvent setup m (AnyEvent e) (Component d runio su sd somehandlers) = do
-    ((runlog, model'), d') <- exec [] m d $ somehandlers d
-    return $ ((runlog, model'), Component d' runio su sd somehandlers)
+dispatchAnyEvent :: MonadIO m => AnyEvent -> Component -> Z m Component
+dispatchAnyEvent (AnyEvent e) (Component cid d runio su sd handlers) = do
+    model <- get
+    ((l, model'), d') <- exec model d [] $ handlers d
+    put model'
+    when (not $ null l) $ appendLog $ [cid ++ ":"] ++ map ("\t"++) l
+    return $ Component cid d' runio su sd handlers
+
     where
-    exec runlog model cdata []     = return ((runlog, model), cdata)
-    exec runlog model cdata (h:hs) = do
-        ((runlog', model'), cdata') <- run model cdata h
-        exec (runlog ++ runlog') model' cdata' hs
-    run model cdata h = runio (execStack (dispatch e h) setup model) cdata
+    exec model cdata l []     = return ((l, model), cdata)
+    exec model cdata l (h:hs) = do
+        ((l', model'), cdata') <- run model cdata h
+        exec model' cdata' (l ++ l') hs
+
+    run model cdata h = do
+        setup <- ask
+        io $ runio (execStack setup model (dispatch e h)) cdata
 
 
-dispatchAnyEvent :: AnyEvent -> Component -> Z IO Component
-dispatchAnyEvent (AnyEvent e) (Component d runio su sd handlers) = do
-    (runlog, d') <- exec [] d $ handlers d
-    io $ printLog runlog
-    -- toLog $ showLog runlog
-    return $ Component d' runio su sd handlers
+runController :: [SetupRT IO AnyEvent] -> SetupRT IO [(ThreadId, TChan AnyEvent)]
+runController = mapM run
     where
-    exec runlog cdata []     = return (runlog, cdata)
-    exec runlog cdata (h:hs) = do
-        (runlog', cdata') <- run cdata h
-        -- toLog runlog'
-        exec (runlog ++ runlog') cdata' hs
-
-    run cdata h = do
-        s <- ask
-        m <- get
-        ((rlog, m'), cdata') <- io $ runio (execStack (dispatch e h) s m) cdata
-        put m'
-        return (rlog, cdata')
-
-
-runController :: Setup -> [Setup -> IO AnyEvent] -> IO [(ThreadId, TChan AnyEvent)]
-runController setup = mapM run
-    where
-    run f = do
+    run f = ask >>= \setup -> io $ do
         chan <- newTChanIO
-        tid <- forkIO $ forever $ f setup >>= atomically . writeTChan chan
+        tid <- forkIO . forever $ runReaderT f setup >>= atomically . writeTChan chan
         return (tid, chan)
 
 
-runConsumers :: [TChan AnyEvent] -> [Component] -> Z IO ()
-runConsumers chans components = readAnyEvent >>= run components
+runComponentsOnce :: MonadIO m => [TChan AnyEvent] -> [Component] -> Z m [Component]
+runComponentsOnce chans = (readAnyEvent >>=) . run
     where
-    run cs ae = mapM (dispatchAnyEvent ae) cs >>= (readAnyEvent >>=) . run
+    run cs ae = mapM (dispatchAnyEvent ae) cs
     readAnyEvent = io . atomically . foldr1 orElse . map readTChan $ chans
