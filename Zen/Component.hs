a3db8636139f2e93d2eedb171c08715c7b41621f
Jochen Keil <jochen.keil@gmail.com>
2014-07-19 11:35:11 +0200

    Reduce Component module to minimal necessary functionality

--- Zen/Component.hs
+++ Zen/Component.hs
@@ -1,53 +1,22 @@
 -- vim:sw=4:sts=4:ts=4
 
-{-# OPTIONS_GHC -Wall #-}
-
 module Component where
 
-import Control.Monad.State
-import Control.Monad.Reader
-import Control.Monad.Writer
-import Control.Monad.Catch (bracket)
-
-import Log
-import Lens
-import Util
 import Types
-import Xproto
 
 
-withComponents :: ([Component] -> ModelST (SetupRT IO) a) -> ModelST (SetupRT IO) a
-withComponents = bracket startup shutdown
-
-
-runComponent :: MonadIO c => AnyEvent -> Component c -> c (Component c)
+runComponent :: Monad m => AnyEvent -> Component m -> m (Component m)
 runComponent (AnyEvent e) (Component cid d execc su sd handlers) = do
     d' <- execc (mapM_ dispatchEvent $ handlers d) d
     return $ Component cid d' execc su sd handlers
     where dispatchEvent (SomeHandler h) = dispatch h e
-    where
-    startup = askL (config . components) >>= mapM startupComponent
-    shutdown = mapM_ shutdownComponent
 
 
-startupComponent :: Component -> ModelST (SetupRT IO) Component
+startupComponent :: Component c -> c (Component c)
 startupComponent (Component cid d r startup su h) = do
-    (d', l) <- connection $-> flip runXprotoT (runWriterT (startup d))
-    io . printLog $ ("startup " ++ cid ++ ":") : l
+    d' <- startup d
     return $ Component cid d' r startup su h
 
 
-shutdownComponent :: Component -> ModelST (SetupRT IO) ()
-shutdownComponent (Component cid d _ _ shutdown _) = do
-    l <- connection $-> flip runXprotoT (execWriterT (shutdown d))
-    io . printLog $ ("shutdown " ++ cid ++ ":") : l
-
-
-runStack :: MonadIO m => Setup -> Model -> Z m a -> m ((a, Log), Model)
-runStack setup m f = runReaderT (runStateT (runXprotoT c $ runWriterT f) m) setup
-    where c = setup ^. connection
-
-
-execStack :: MonadIO m => Setup -> Model -> Z m () -> m (Log, Model)
-execStack setup m f = runReaderT (runStateT (runXprotoT c $ execWriterT f) m) setup
-    where c = setup ^. connection
+shutdownComponent :: Component c -> c ()
+shutdownComponent (Component _ d _ _ shutdown _) = shutdown d
