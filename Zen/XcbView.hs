e4adcd545eea4bebf335bedffb925373f5cdc0d0
Jochen Keil <jochen.keil@gmail.com>
2014-07-19 23:50:48 +0200

    Implement XcbView for latest Model

--- Zen/XcbView.hs
+++ Zen/XcbView.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE FlexibleContexts, LambdaCase #-}
+{-# LANGUAGE LambdaCase #-}
 
 module XcbView where
 
 import Data.Map as M
 import Data.Set as S
 import Data.List as L
+import Control.Monad.State
 import Control.Monad.Reader
 import Graphics.XHB as X hiding (Setup)
 
@@ -13,20 +14,20 @@ import Util
 import Model
 import Types
 
-xcbView :: (MonadIO m, MonadReader Setup m) => Model -> UpdateHints -> m ()
-xcbView _ = mapM_ (uncurry configure) . M.toList
-          . M.map (toValueParam . concat . L.map hintToValueParam . S.toList)
+
+xcbView :: View
+xcbView configs = do
+    model <- ask
+    io . putStrLn $ "xcbView:\n" ++ show model
+    mapM_ configure
+        . M.toList . M.map (toValueParam . L.nub . L.map convertConfigs . S.toList) $ configs
     where
-    configure w vp = connection $-> \c -> io $ X.configureWindow c w vp
-    hintToValueParam = \case
-        HintX v -> [(ConfigWindowX, fi v)]
-        HintY v -> [(ConfigWindowY, fi v)]
-        HintWidth v -> [(ConfigWindowWidth, fi v)]
-        HintHeight v -> [(ConfigWindowHeight, fi v)]
-        HintPosition p -> [(ConfigWindowX, fi $ p ^. x), (ConfigWindowY, fi $ p ^. y)]
-        HintDimension d -> [(ConfigWindowWidth, fi $ d ^. width), (ConfigWindowHeight, fi $ d ^. height)]
-        _ -> []
-        -- HintRaise w
-        -- HintLower w
-        -- HintBorderColor v) w
-        -- HintBorderWidth v) w
+    configure (w, vp) = lift (askL connection) >>= \c -> do
+        io $ putStrLn $ "configure " ++ show w ++ " with " ++ show vp
+        io $ X.configureWindow c w vp
+
+    convertConfigs = \case
+        ConfigClientX      _ v _ -> (ConfigWindowX, fi v)
+        ConfigClientY      _ v _ -> (ConfigWindowY, fi v)
+        ConfigClientWidth  _ v _ -> (ConfigWindowWidth, fi v)
+        ConfigClientHeight _ v _ -> (ConfigWindowHeight, fi v)
